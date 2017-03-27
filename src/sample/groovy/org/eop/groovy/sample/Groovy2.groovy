afddbf2beea5c242adf7a3c2adf72389e797212b
lixinjie <lixinjie1985@126.com>
2017-03-27 15:16:00 +0800

    update15:15

--- src/sample/groovy/org/eop/groovy/sample/Groovy2.groovy
+++ src/sample/groovy/org/eop/groovy/sample/Groovy2.groovy
@@ -8,7 +8,16 @@ class Groovy2 {
 	static main(args) {
 		println 'import时使用as关键字指定别名，可以解决命名冲突的问题，import java.sql.Date as SqlDate'
 		println 'line 667 of groovy.txt---------------------------------'
-		
+		def (a, b, c) = [1, 2, 3]
+		println a
+		println b
+		println c
+		def (_, e) = [1, 2, 3, 4]
+		println _
+		println e
+		def (g, h) = [1]
+		println g
+		println h
 	}
 
 }
